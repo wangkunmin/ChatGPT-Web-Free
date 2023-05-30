13db2a4f662c6558aacc99724e23fa124a9d98d1
wangkm <wangkunmin@xiaopin.com>
2023-05-30 14:44:27 +0800

    fix github

--- src/components/common/UserAvatar/index.vue
+++ src/components/common/UserAvatar/index.vue
@@ -7,13 +7,13 @@ import { GithubSite } from '@/components/custom'
     <div class="w-10 h-10 overflow-hidden rounded-full">
       <img class="object-cover" src="@/assets/avatar.jpg" alt="avatar">
     </div>
-    <div class="ml-2">
+<!--    <div class="ml-2">
       <h2 class="font-bold text-md">
         ChenZhaoYu
       </h2>
       <p class="text-xs text-gray-500">
         <GithubSite />
       </p>
-    </div>
+    </div>-->
   </div>
 </template>
--- src/components/custom/GithubSite.vue
+++ src/components/custom/GithubSite.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="text-center text-neutral-500">
     <span>❤️ Star on</span>
-    <a href="https://github.com/Chanzhaoyu/chatgpt-bot" target="_blank" class="text-blue-500">
+<!--    <a href="https://github.com/Chanzhaoyu/chatgpt-bot" target="_blank" class="text-blue-500">
       GitHub
-    </a>
+    </a>-->
   </div>
 </template>
