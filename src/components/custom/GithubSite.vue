034ba6ac03213fd133268dca6dfb6c4b5f8f4019
wangkunmin <2239486439@qq.com>
2023-05-31 14:45:50 +0800

    Update GithubSite.vue

--- src/components/custom/GithubSite.vue
+++ src/components/custom/GithubSite.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="text-center text-neutral-500">
     <span>❤️ Star on</span>
-    <a href="https://github.com/wangkunmin/ChatGPT-Web-Free" target="_blank" class="text-blue-500">
+    <a href="" target="_blank" class="text-blue-500">
       GitHub
     </a>
   </div>
